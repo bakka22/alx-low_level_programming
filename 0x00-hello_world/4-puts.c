fa433caf178ec53c7e75586791d668b03ddbecd8
bakka22 <abobakereltom@gmail.com>
2023-07-14 01:12:18 +0000

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/** main - print a string on terminal */
+/**
+* main - the main function
+* Descriptio : prints a string on screen
+*/
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
