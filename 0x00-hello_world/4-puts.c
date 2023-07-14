3992a08cd2c3e97340645245c895d9b6fc03c03b
bakka22 <abobakereltom@gmail.com>
2023-07-14 01:20:33 +0000

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - the main function
-* Descriptio : prints a string on screen
+* Return: 0 (success)
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle,");
 	return (0);
 }
