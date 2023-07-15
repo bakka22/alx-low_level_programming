faf5038862cc2ae7d59d754de9a08495fbea6a0c
bakka22 <abobakereltom@gmail.com>
2023-07-15 11:48:54 +0000

    s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - the main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+	int x;
+	int j;
+
+	for (n = 48 ; n <= 55 ; n++)
+	{
+		for (x = n + 1 ; x <= 56 ; x++)
+		{
+			for (j = x + 1 ; x <= 57 ; j++)
+			putchar(n);
+			putchar(x);
+			putchar(j);
+			if (n != 55)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
