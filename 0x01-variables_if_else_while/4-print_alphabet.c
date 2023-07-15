ca72ccba750b88c0294cf246ec0f1027ce7d0c55
bakka22 <abobakereltom@gmail.com>
2023-07-15 01:03:24 +0000

    d

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - the main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 97 ; n <= 122 ; n++)
+		if (n != 113 || n != 101)
+			putchar(n);
+
+	putchar('\n');
+	return (0);
+}
