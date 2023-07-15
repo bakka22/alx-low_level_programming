ba84a2fcd1f3a01a72b6633c9abe3758b5256d4d
bakka22 <abobakereltom@gmail.com>
2023-07-15 01:25:50 +0000

    d

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - the main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 122 ; n <= 97 ; n--)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
