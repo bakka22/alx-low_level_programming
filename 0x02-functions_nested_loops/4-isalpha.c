60d31354bac17523e4cbf3ff731a60b75f778d91
bakka22 <abobakereltom@gmail.com>
2023-07-18 23:07:20 +0000

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _islower - check if charcter is an alphabet
+* @c: the number to be checked
+* Return: 1 if alphabet and 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
