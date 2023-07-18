14582ed54e0bfbae8e7dcd2afb55442fbdeebb08
bakka22 <abobakereltom@gmail.com>
2023-07-18 19:51:20 +0000

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h>
+/* more headers goes there */
+/**
+*main - the main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+*print_alphabet - print alphabets in lowercase
+* Return: void
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97 ; i <= 122 ; i++)
+		putchar(i);
+
+	putchar(\n);
+}
