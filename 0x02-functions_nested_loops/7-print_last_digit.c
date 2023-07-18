dcd8cafddaa3e9d89dfded52204421a14c8b0f7b
bakka22 <abobakereltom@gmail.com>
2023-07-18 23:34:51 +0000

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_last_digit - check if charcter is an alphabet
+* @i: the number under operation
+* Return: last digit
+*/
+int print_last_digit(int i)
+{
+	return (i % 10);
+
+}
