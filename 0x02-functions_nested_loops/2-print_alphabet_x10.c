5289b9847030717d1f967a3fc06e9410191e5bd9
bakka22 <abobakereltom@gmail.com>
2023-07-18 20:02:10 +0000

    S

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_alphabet - print alphabets in lowercase
+* Return: void
+*/
+void print_alphabet(void)
+{
+	int i;
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (i = 97 ; i <= 122 ; i++)
+			putchar(i);
+	}
+
+	putchar('\n');
+}
