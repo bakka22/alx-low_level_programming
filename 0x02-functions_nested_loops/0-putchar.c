8618b8cc09c20af76f18def326b1f218fe6e20ea
bakka22 <abobakereltom@gmail.com>
2023-07-18 19:36:51 +0000

    S

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/* more headers goes there */
+/**
+*main - the main function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
