bce0d73a175f1a2435b07ea2720a27c022e19999
bakka22 <abobakereltom@gmail.com>
2023-07-19 00:42:02 +0000

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*jack_bauer - print alphabets in lowercase
+* Return: void
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+
+	for (i = 48 ; i <= 54 ; i++)
+	{
+		for (j = 48 ; j <= 57 ; j++)
+		{
+			for (x = 48 ; x <= 54 ; x++)
+			{
+				for (y = 48 ; y <= 57 ; y++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(':');
+					putchar(x);
+					putchar(y);
+					putchar(\n);
+				}
+			}
+		}
+	}
+
+}
