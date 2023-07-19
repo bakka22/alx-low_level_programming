9f9c43e2da555b79c0793d957a892933780d1c1b
bakka22 <abobakereltom@gmail.com>
2023-07-19 15:00:18 +0000

    s

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,8 +7,10 @@
 */
 void print_times_table(int n)
 {
-	int i, j, x;
+	int i, j, x, y, k;
 
+	y = x / 100;
+	k = (x / 10) % 10;
 	if (n < 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -30,16 +32,12 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(x / 10 + '0');
-					_putchar(x % 10 + '0');
-				}
-				else if (x >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(x / 100 + '0');
-					_putchar((x / 10) % 10 + '0');
+					if (x >= 100)
+						_putchar(y = '0');
+					else
+						_putchar(' ');
+
+					_putchar(k + '0');
 					_putchar(x % 10 + '0');
 				}
 			}
