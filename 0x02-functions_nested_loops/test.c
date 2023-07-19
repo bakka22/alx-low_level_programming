51b557487500bbb896c7b01971fc9bb0a8ef6dde
bakka22 <abobakereltom@gmail.com>
2023-07-19 13:47:31 +0000

    s

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-int main (void) 
+#include "main.h"
+
+/**
+*times_table - print alphabets in lowercase
+* Return: void
+*/
+void times_table(void)
 {
-printf("hello"); 
-printf("%lu", sizeof(int));
-return 0;
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar((k / 10) + '0');
+				putchar((k % 10) + '0');
+			}
+		}
+		putchar('\n');
+
+	}
+}
+int main (void)
+{
+	times_table();
+	return 0;
 }
