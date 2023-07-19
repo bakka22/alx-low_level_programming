9f65bcd4c3bd28bc7f8fc3e93dfce056d38c5218
bakka22 <abobakereltom@gmail.com>
2023-07-19 00:01:20 +0000

    d

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,14 @@
 */
 int print_last_digit(int i)
 {
-	if ( i < 0)
+	if (i < 0)
 		i = i * -1;
+	if (i < 10)
+		i = i
+	else
+		i = i % 10
 
-	_putchar((i % 10) + '0');
-	return (i % 10);
+	_putchar((i) + '0');
+	return (i);
 
 }
