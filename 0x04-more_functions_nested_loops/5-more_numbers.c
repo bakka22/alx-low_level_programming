8006411d27f1ecd018f8e917c472e6d3afba7afc
bakka22 <abobakereltom@gmail.com>
2023-07-20 20:15:10 +0000

    s

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* more_numbers - check if charcter is an alphabet
+* Return: 1 if alphabet and 0 otherwise
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (i == 0; i <= 10; i++)
+		{
+			for (j = 0; j <= 14; j++)
+			{
+				if (j < 10)
+					_putchar(j + '0');
+				else if (j >= 10)
+				{
+					_putchar(j / 10 + '0');
+					_putchar(j % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+
+}
