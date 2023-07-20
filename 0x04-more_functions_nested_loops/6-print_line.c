6f5c1ff0210aa2a9c90a5a2f2435c0e16cf9412b
bakka22 <abobakereltom@gmail.com>
2023-07-20 22:54:19 +0000

    d

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - check if charcter is an alphabet
+* @n: the length of the line
+* Return: 1 if alphabet and 0 otherwise
+*/
+void print_line(int n)
+{
+	int i:
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+
+}
