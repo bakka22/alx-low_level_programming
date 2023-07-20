7503ce9df7b83a574d92c0e2a8fd5d8f80d38dc2
bakka22 <abobakereltom@gmail.com>
2023-07-20 19:28:41 +0000

    s

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,10 @@
 * print_numbers - check if charcter is an alphabet
 * Return: 1 if alphabet and 0 otherwise
 */
-int print_numbers(void)
+void print_numbers(void)
 {
-	int i:
+	int i;
 
 	for (i = 48; i <= 57; i++)
 		_putchar(i);
-
-		return (0);
 }
