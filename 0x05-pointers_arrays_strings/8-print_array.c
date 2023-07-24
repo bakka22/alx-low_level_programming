4d96b3585f9074918cdf52037f98405c1cb1fcee
bakka22 <abobakereltom@gmail.com>
2023-07-24 23:40:00 +0000

    d

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,14 +16,42 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			_putchar(*p);
+			print_number(*P);
 		}
 		else
 		{
-			_putchar(*p);
+			print_number(*P);
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
 	_putchar('\n');
+}
+#include "main.h"
+
+/**
+* print_number - check if charcter is an alphabet
+* @n: the length of the line
+* Return: 1 if alphabet and 0 otherwise
+*/
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar(n1 % 10 + '0');
 }
