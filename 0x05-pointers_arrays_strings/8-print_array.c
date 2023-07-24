e61eaa953e46897bf460451b53e7cdf78d7ed309
bakka22 <abobakereltom@gmail.com>
2023-07-24 23:32:52 +0000

    s

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,11 +16,11 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			_putchar(*P);
+			_putchar(*p);
 		}
 		else
 		{
-			_putchar(*P);
+			_putchar(*p);
 			_putchar(',');
 			_putchar(' ');
 		}
