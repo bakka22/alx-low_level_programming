67d40b1757fc33b16d1c6ab8126ab5d56b8dca2f
bakka22 <abobakereltom@gmail.com>
2023-07-26 22:20:57 +0000

    s

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,5 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
+	if (n >= _strlen(src))
+	{
+		for (i = i + 1; dest[i] != '\0'; i++)
+			dest[i] = 0;
+	}
 	return (dest);
 }
