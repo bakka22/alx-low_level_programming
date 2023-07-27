ad2b99fd0e2b0cd8653f1f88bf63d0fa9fb740de
bakka22 <abobakereltom@gmail.com>
2023-07-27 20:40:58 +0000

    s

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* rot13 - check if charcter is an alphabet
+* @a : pointer argument
+* Return: 1 if alphabet and 0 otherwise
+*/
+char *rot13(char *a)
+{
+	unsigned long int i;
+	char ori[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+	char des[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	char *ptr;
+
+	ptr = a;
+	while (a)
+	{
+		for (i = 0; i < sizeof(ori) / sizeof(char); i++)
+		{
+			if (*ptr == ori[i])
+			{
+				*ptr = des[i];
+			}
+		}
+		ptr += 1;
+	}
+	return (a);
+}
+
--- 0x06-pointers_arrays_strings/main.h~
+++ 0x06-pointers_arrays_strings/main.h~
@@ -2,7 +2,7 @@ int _putchar();
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strcpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void revese_array(int *a, int n);
 char *string_toupper(char *);
