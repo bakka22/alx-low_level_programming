1e13100263fbd8f81f3abc3c9b5e586feb5a2ec1
bakka22 <abobakereltom@gmail.com>
2023-07-31 19:14:42 +0000

    S

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+/**
+* _strpbrk - check if charcter is an alphabet
+* @s : pointer argument
+* @accept : pointer argument
+* Return: 1 if alphabet and 0 otherwise
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (;; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+		if (s[i] == '\0')
+			return (NULL);
+	}
+	return (NULL);
+}
