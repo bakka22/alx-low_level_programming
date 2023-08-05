2b0b816e4b31b624dc5b22e4d229f0c6f7341ba2
bakka22 <abobakereltom@gmail.com>
2023-08-05 00:00:51 +0000

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	result = _atoi(argv[1]) + _atoi(argv[2]);
+	result = _atoi(argv[1]) * _atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
 }
