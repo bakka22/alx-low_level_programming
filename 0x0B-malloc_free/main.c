8fb65a5cf8d6a1f47aaf313b5f588c9cf2306e94
bakka22 <abobakereltom@gmail.com>
2023-08-08 22:09:41 +0000

    s

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -38,16 +38,16 @@ int main(void)
 {
     int **grid;
 
-    grid = alloc_grid(6, 4);
+    grid = alloc_grid(70, 70);
     if (grid == NULL)
     {
         return (1);
     }
-    print_grid(grid, 6, 4);
+    print_grid(grid, 10, 10);
     printf("\n");
     grid[0][3] = 98;
     grid[3][4] = 402;
-    print_grid(grid, 6, 4);
+    print_grid(grid, 10, 10);
 	free(grid);
     return (0);
 }
