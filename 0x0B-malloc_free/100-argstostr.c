01ab594d37df597bb8c678e8fa76b74cebd04f3c
bakka22 <abobakereltom@gmail.com>
2023-08-08 23:06:58 +0000

    s

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+* _strlen - check if charcter is an alphabet
+* @s : pointer argument
+* Return: 1 if alphabet and 0 otherwise
+*/
+int _strlen(char *s)
+{
+	char com, *h;
+	int counter;
+
+	counter = 0;
+	h = s;
+	com = *h;
+	while (1)
+	{
+		if (com == '\0')
+			break;
+
+		counter += 1;
+		h += 1;
+		com = *h;
+	}
+	return (counter);
+}
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* argstostr - check if charcter is an alphabet
+* @ac : pointer argument
+* @av : pointer
+* Return: 1 if alphabet and 0 otherwise
+*/
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, x, y, s, *slen;
+
+	if (av == NULL || ac <= 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			av[i] = "";
+	}
+	slen = malloc(sizeof(int) * ac);
+	if (slen == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		slen[i] = _strlen(av[i]);
+		s += _strlen(av[i]);
+	}
+
+	ptr = malloc(s * sizeof(char) + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	y = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (x = 0; x < slen[i]; x++)
+		{
+			ptr[y] = av[i][x];
+			y++;
+		}
+		ptr[y] = '\n';
+		y++;
+	}
+	free(slen);
+	return (ptr);
+}
