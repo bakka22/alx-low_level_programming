10059e68e2a7052641832b1bcb0b55ab3cf58645
bakka22 <abobakereltom@gmail.com>
2023-08-12 00:05:28 +0000

    s

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptr2 = malloc(new_size);
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		*((char *)ptr2 + i) = *((int *)ptr + i);
+		*((char *)ptr2 + i) = *((char *)ptr + i);
 	}
 	free(ptr);
 	return (ptr2);
