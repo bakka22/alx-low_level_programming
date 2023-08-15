86e6616fe53012802d7796eb5e147c3aaf34dac1
bakka22 <abobakereltom@gmail.com>
2023-08-15 00:54:06 +0000

    s

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+*print_dog - initialize a dog var
+*@d : pointer
+*Return: nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if ((*d).name == NULL || ((*d).name)[0] == '\0')
+		(*d).name = "nil";
+	if ((*d).owner == NULL || ((*d).owner)[0] == '\0')
+		(*d).name = "nil";
+	/*
+*if ((*d).age == NULL)
+*	{
+*		printf("Name : %s\nAge : %s\nOwner : %s\n", (*d).name, "nil", (*d).owner);
+*		return;
+*	}
+*/
+	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
