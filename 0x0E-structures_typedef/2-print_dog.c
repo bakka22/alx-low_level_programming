b47d5917893b18d50026f949909fcf30cf178724
bakka22 <abobakereltom@gmail.com>
2023-08-15 00:59:27 +0000

    s

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,19 +10,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf(" ");
 		return;
 	}
 	if ((*d).name == NULL || ((*d).name)[0] == '\0')
 		(*d).name = "nil";
 	if ((*d).owner == NULL || ((*d).owner)[0] == '\0')
 		(*d).name = "nil";
-	/*
-*if ((*d).age == NULL)
-*	{
-*		printf("Name : %s\nAge : %s\nOwner : %s\n", (*d).name, "nil", (*d).owner);
-*		return;
-*	}
-*/
+	if ((*d).age == 0)
+	{
+		printf("Name : %s\nAge : %s\nOwner : %s\n", (*d).name, "nil", (*d).owner);
+		return;
+	}
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
