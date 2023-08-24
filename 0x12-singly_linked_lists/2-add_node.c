8641119e12bf38a9234d4ce606882c4b8bca6a3a
bakka22 <abobakereltom@gmail.com>
2023-08-24 18:04:32 +0000

    s

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+/**
+* _strlen - check if charcter is an alphabet
+* @s : pointer argument
+* Return: 1 if alphabet and 0 otherwise
+*/
+int _strlen(char *s)
+{
+	char com, *h;
+	int counter;
+
+	counter = 0;
+	h = s;
+	com = *h;
+	while (1)
+	{
+		if (com == '\0')
+			break;
+
+		counter += 1;
+		h += 1;
+		com = *h;
+	}
+	return (counter);
+}
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+*add_node - add a new list node at head
+*@head :the head of the node
+*@str :string on the node
+*Return: a pointer to the head of the list
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
