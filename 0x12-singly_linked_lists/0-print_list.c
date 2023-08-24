8017f11a2cbc34e2c34c8c2d18b3a8627e0cae0a
bakka22 <abobakereltom@gmail.com>
2023-08-24 15:18:58 +0000

    s

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,25 +8,24 @@
 */
 size_t print_list(const list_t *h)
 {
-	list_t *tmp;
 	size_t count;
 
 	if (h == NULL)
 		return (0);
 	count = 0;
-	while (tmp != NULL)
+	while (h != NULL)
 	{
-		if (tmp->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 			count++;
 		}
 		else
 		{
-			printf("[%d] %s", tmp->len, tmp->str);
+			printf("[%d] %s\n", h->len, h->str);
 			count++;
 		}
-		tmp = tmp->next;
+		h = h->next;
 	}
 	return (count);
 }
