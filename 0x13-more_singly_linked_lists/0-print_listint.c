ac9c48aabaa5f4dc0f5a51f1f6b8b251a8e56ea6
bakka22 <abobakereltom@gmail.com>
2023-08-28 07:18:38 +0000

    s

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+*print_listint - print a linked list
+*@h : head of the list
+*Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
