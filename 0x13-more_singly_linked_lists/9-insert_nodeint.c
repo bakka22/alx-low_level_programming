0e66a49ea4e85e3a4a56ea8a82c70c86ea0bad79
bakka22 <abobakereltom@gmail.com>
2023-08-29 10:30:38 +0000

    e

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -48,7 +48,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (*head != NULL)
 		{
-			tmp = (*head)->next;
+			tmp = *head;
 			*head = new;
 			new->next = tmp;
 		}
