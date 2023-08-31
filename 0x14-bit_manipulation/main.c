57450178cdfeff5cc44fe1f4b54dcc14989d46a2
bakka22 <abobakereltom@gmail.com>
2023-08-31 12:52:16 +0000

    S

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -8,15 +8,13 @@
  */
 int main(void)
 {
-    print_binary(0);
-    printf("\n");
-    print_binary(INT_MAX);
-    printf("\n");
-    print_binary(INT_MIN);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
+    int n;
+
+    n = get_bit(-1, 63);
+    printf("%d\n", n);
+    n = get_bit(-1, 0);
+    printf("%d\n", n);
+    n = get_bit(-1, 64);
+    printf("%d\n", n);
     return (0);
 }
