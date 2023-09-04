e2fb201a529e4cc0b0a9199409ad0798081e08cf
bakka22 <abobakereltom@gmail.com>
2023-09-04 21:46:36 +0000

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,11 +18,11 @@ void error(int ind, char **av)
 			exit(97);
 			break;
 		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 			break;
 		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 			exit(99);
 	}
 }
@@ -64,13 +64,13 @@ int main(int ac, char **av)
 	clx = close(x);
 	if (clx < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", x);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
 		exit(100);
 	}
 	cly = close(y);
 	if (cly < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", y);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", y);
 		exit(100);
 	}
 	return (0);
