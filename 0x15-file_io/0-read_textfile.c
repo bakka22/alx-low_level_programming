9309abfebe55c68ae9978599192253a3b70cd304
bakka22 <abobakereltom@gmail.com>
2023-09-04 15:25:17 +0000

    s

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcnlt.h>
+/**
+*read_textfile - reads a text file
+*@filename : the name of the file
+*@letters : the number of letters to read
+*Return: the actual number of letters it could read
+*/ 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t count;
+	int x;
+	char buffer;
+
+	count = 0;
+	if (filename == NULL)
+		return (0);
+	x = open(filename, O_RDONLY);
+	if (x < 0)
+		return (0);
+	while (read(x, &buffer, 1) && letters)
+	{
+		letters--;
+		count++;
+		write(1, &buffer, 1);
+	}
+	return (count);
+}
