6c0b245a5ad8a663bb2f2d401290a54796bc6c07
bakka22 <abobakereltom@gmail.com>
2023-09-04 18:02:44 +0000

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	x = open(filename, O_RDWR | O_CREAT | O_DSYNC, 0600);
+	x = open(filename, O_RDWR | O_CREAT | O_DSYNC | O_TRUNC, 0600);
 	if (x == -1)
 		return (-1);
 	if (text_content != NULL)
