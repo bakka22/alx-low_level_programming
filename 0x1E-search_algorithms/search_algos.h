cfa08c4ea4b389fbaf910c97ba5d52e29778c412
bakka22 <abobakereltom@gmail.com>
2024-03-13 00:24:12 +0000

    s

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,10 @@
 #ifndef ALGOS_H
 #define ALGOS_H
+#include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
